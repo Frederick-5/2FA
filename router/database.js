623bc79ff177d60f090f4ab5dbe6ffcec2ffa559
Frederick-5 <110483095+Frederick-5@users.noreply.github.com>
2024-02-24 14:49:29 +0100

    first commit

---
+++ router/database.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+require("dotenv").config()
+
+mongoose.connect(process.env.DB)
+
+const Schema = new mongoose.Schema({
+    fullname: String,
+    username: String,
+    email: String,
+    password: String,
+    TFA_enabled: Boolean,
+    unique: String,
+    secret: String
+})
+
+const User = mongoose.model("Clients", Schema)
+module.exports = {User, bcrypt}
